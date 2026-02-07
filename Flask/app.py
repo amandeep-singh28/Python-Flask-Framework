21b1179139408f6b3ae408fdfd48e3a37783d15e
Amandeep Singh <amandeep87557@gmail.com>
2026-02-07 10:39:40 +0530

    How to use Python variables and Conditional statement using JINJA templates

--- Flask/app.py
+++ Flask/app.py
@@ -1,25 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-    return render_template("login.html")
-
-
-@app.route("/submit", methods = ["POST"])
-def submit():
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    # if username == "aman123" and password == "pass":
-    #     return render_template("welcome.html", name = username)
-    valid_user = {
-        'amandeep' : '123',
-        'aman' : '123',
-        'amandeep singh' : '123'
-    }
-    if username in valid_user and password == valid_user[username]:
-        return render_template("welcome.html", name = username)
-    else:
-        return "Invalid credentials"
+def student_profile():
+    return render_template(
+        "profile.html",
+        name = "Amandeep",
+        is_topper = True,
+        subjects = ["Maths", "Science", "History"]
+    )
